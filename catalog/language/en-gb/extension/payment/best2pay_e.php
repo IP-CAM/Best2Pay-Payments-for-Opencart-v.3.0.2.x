afbe337ec61fbc39c95e2653b0e976749f302aa3
Алексей Строков <alex-weirdo@yandex.ru>
2021-04-22 17:25:23 +0300

    getting notifications from b2p

--- catalog/language/en-gb/extension/payment/best2pay_e.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$_['text_title']					= 'Digital money via Best2Pay';
-$_['text_error']					= '<b>There was an error. Please contact the on-line shop.</b>';
-$_['order_number']        			= 'Order';
-
